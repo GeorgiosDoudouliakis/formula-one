5cceaa7f9b727a6b47369442b50e0a1e0f11e6b9
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-02-10 16:50:36 +0200

    feat: Change header of home page depending on toggle

--- src/app/modules/home/components/toggle/toggle.component.ts
+++ src/app/modules/home/components/toggle/toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
@@ -7,6 +7,7 @@ import { MatSlideToggleChange } from '@angular/material/slide-toggle';
   styleUrls: ['./toggle.component.scss']
 })
 export class ToggleComponent implements OnInit {
+  @Output() optionHandler = new EventEmitter();
   option: 'Driver' | 'Constructor' = 'Driver';
 
   constructor() { }
@@ -15,5 +16,6 @@ export class ToggleComponent implements OnInit {
 
   onOptionChange(event: MatSlideToggleChange) {
     this.option = event.checked ? 'Constructor' : 'Driver';
+    this.optionHandler.emit(this.option);
   }
 }
