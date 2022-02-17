16c5fc612a398e532b484dd77c3c97afcd531ef4
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-02-17 16:43:08 +0200

    fix: Not existing constructor and driver image

--- src/app/shared/components/constructors-drivers/constructors-drivers.component.ts
+++ src/app/shared/components/constructors-drivers/constructors-drivers.component.ts
@@ -1,31 +1,26 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { YearHandlerService } from '@shared/services/year-handler.service';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-constructors-drivers',
   templateUrl: './constructors-drivers.component.html',
-  styleUrls: ['./constructors-drivers.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./constructors-drivers.component.scss']
 })
 export class ConstructorsDriversComponent implements OnInit, OnDestroy {
   @Input() type: 'drivers' | 'constructors';
   @Input() data$: Observable<any>;
-  year: string;
-  private yearSub$: Subscription;
+  data: any[];
+  private dataSub$: Subscription;
 
-  constructor(
-    public router: Router,
-    private yearHandlerService: YearHandlerService
-  ) { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
-    this.yearSub$ = this.yearHandlerService.year$.subscribe((year: string) => this.year = year);
+    this.dataSub$ = this.data$.subscribe(res => this.data = res);
   }
 
   ngOnDestroy() {
-    this.yearSub$?.unsubscribe();
+    this.dataSub$?.unsubscribe();
   }
 
   navigateTo(data: any) {
@@ -34,5 +29,41 @@ export class ConstructorsDriversComponent implements OnInit, OnDestroy {
     } else if(this.type === 'constructors') {
       this.router.navigate(['/constructors', data.constructorId]);
     }
+  }
+
+  imageExists(details: any) {
+    if(this.type === 'drivers') {
+      return details?.driverId === 'alonso' ||
+             details?.driverId === 'bottas' ||
+             details?.driverId === 'gasly'  ||
+             details?.driverId === 'giovinazzi' ||
+             details?.driverId === 'hamilton' ||
+             details?.driverId === 'kubica' ||
+             details?.driverId === 'latifi' ||
+             details?.driverId === 'leclerc' ||
+             details?.driverId === 'mazepin' ||
+             details?.driverId === 'norris' ||
+             details?.driverId === 'ocon' ||
+             details?.driverId === 'perez' ||
+             details?.driverId === 'raikkonen' ||
+             details?.driverId === 'ricciardo' ||
+             details?.driverId === 'russell' ||
+             details?.driverId === 'sainz' ||
+             details?.driverId === 'mick_schumacher' ||
+             details?.driverId === 'stroll' ||
+             details?.driverId === 'tsunoda' ||
+             details?.driverId === 'max_verstappen' ||
+             details?.driverId === 'vettel';
+    } 
+    return  details?.constructorId === 'alphatauri' ||
+            details?.constructorId === 'alfa' ||
+            details?.constructorId === 'alpine' ||
+            details?.constructorId === 'aston_martin' ||
+            details?.constructorId === 'ferrari' ||
+            details?.constructorId === 'haas' ||
+            details?.constructorId === 'mclaren' ||
+            details?.constructorId === 'mercedes' ||
+            details?.constructorId === 'red_bull' ||
+            details?.constructorId === 'williams'; 
   }
 }
--- src/app/shared/components/driver-constructor/driver-constructor.component.ts
+++ src/app/shared/components/driver-constructor/driver-constructor.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { YearHandlerService } from '@shared/services/year-handler.service';
-import { Subscription } from 'rxjs';
 import { Race } from '../../models/round-standings.model';
 
 @Component({
@@ -12,16 +10,44 @@ export class DriverConstructorComponent implements OnInit {
   @Input() type: 'driver' | 'constructor';
   @Input() details: any;
   @Input() results: Race[];
-  year: string;
-  private yearSub$: Subscription;
 
-  constructor(private yearHandlerService: YearHandlerService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.yearSub$ = this.yearHandlerService.year$.subscribe((year: string) => this.year = year);
-  }
+  ngOnInit(): void {}
 
-  ngOnDestroy() {
-    this.yearSub$?.unsubscribe();
+  get imageExists() {
+    if(this.type === 'driver') {
+      return this.details?.driverId === 'alonso' ||
+             this.details?.driverId === 'bottas' ||
+             this.details?.driverId === 'gasly'  ||
+             this.details?.driverId === 'giovinazzi' ||
+             this.details?.driverId === 'hamilton' ||
+             this.details?.driverId === 'kubica' ||
+             this.details?.driverId === 'latifi' ||
+             this.details?.driverId === 'leclerc' ||
+             this.details?.driverId === 'mazepin' ||
+             this.details?.driverId === 'norris' ||
+             this.details?.driverId === 'ocon' ||
+             this.details?.driverId === 'perez' ||
+             this.details?.driverId === 'raikkonen' ||
+             this.details?.driverId === 'ricciardo' ||
+             this.details?.driverId === 'russell' ||
+             this.details?.driverId === 'sainz' ||
+             this.details?.driverId === 'mick_schumacher' ||
+             this.details?.driverId === 'stroll' ||
+             this.details?.driverId === 'tsunoda' ||
+             this.details?.driverId === 'max_verstappen' ||
+             this.details?.driverId === 'vettel';
+    } 
+    return  this.details?.constructorId === 'alphatauri' ||
+            this.details?.constructorId === 'alfa' ||
+            this.details?.constructorId === 'alpine' ||
+            this.details?.constructorId === 'aston_martin' ||
+            this.details?.constructorId === 'ferrari' ||
+            this.details?.constructorId === 'haas' ||
+            this.details?.constructorId === 'mclaren' ||
+            this.details?.constructorId === 'mercedes' ||
+            this.details?.constructorId === 'red_bull' ||
+            this.details?.constructorId === 'williams'; 
   }
 }
