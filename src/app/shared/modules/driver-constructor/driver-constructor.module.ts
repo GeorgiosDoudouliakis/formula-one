1b42c3259679bda206fc32f4015c8c041ef74eb4
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-25 20:05:36 +0300

    refactor: Remove statistics from driver-constructor component and delete ngx-echarts package

--- src/app/shared/modules/driver-constructor/driver-constructor.module.ts
+++ src/app/shared/modules/driver-constructor/driver-constructor.module.ts
@@ -3,10 +3,6 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { CardModule } from "../card/card.module";
 import { DriverConstructorComponent } from './components/driver-constructor/driver-constructor.component';
-import { StatisticsComponent } from "./components/statistics/statistics.component";
-import { NgxEchartsModule } from "ngx-echarts";
-import * as echarts from "echarts";
-import { GraphService } from "./services/graph.service";
 import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
@@ -14,15 +10,9 @@ import { MatIconModule } from "@angular/material/icon";
     CommonModule,
     HttpClientModule,
     CardModule,
-    NgxEchartsModule.forRoot({ echarts }),
     MatIconModule
   ],
-  declarations: [
-    DriverConstructorComponent,
-    StatisticsComponent
-  ],
-  exports: [DriverConstructorComponent],
-  providers: [GraphService]
+  declarations: [DriverConstructorComponent],
+  exports: [DriverConstructorComponent]
 })
-export class DriverConstructorModule {
-}
+export class DriverConstructorModule {}
