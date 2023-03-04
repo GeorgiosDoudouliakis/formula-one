8c7e07c3bc055cd8231bb7d7b5d4ed4bb5b62821
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-03-04 14:13:22 +0200

    refactor: Remove go top button, add box shadow and increase the font size of paragraph

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,30 +1,10 @@
 /* Place angular imports */
-import {Component, Inject} from '@angular/core';
-
-/* Place angular material imports here */
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  imports: [
-    MatButtonModule,
-    MatIconModule
-  ],
-  providers: [
-    {
-      provide: Window,
-      useValue: window
-    }
-  ]
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  constructor(@Inject(Window) private _window: Window) {}
-
-  public goTop(): void {
-    this._window.scrollTo(0, 0);
-  }
-}
+export class FooterComponent {}
