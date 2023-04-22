d4c16eb78a5add53299154b390425d820c337871
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-04-22 14:11:17 +0300

    refactor: Change the layout of page header component, pass selected year as an input, remove styles from standings component and add queries for h1 element

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,14 +1,24 @@
 /* Place angular imports */
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-page-header[title]',
   standalone: true,
-  templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss'],
-  imports: [CommonModule]
+  template: `
+    <h1>
+      F1 {{ title | uppercase }} {{ year }}
+    </h1>
+  `,
+  styles: [`
+    h1 {
+      margin-bottom: 2rem;
+    }
+  `],
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageHeaderComponent {
   @Input() public title!: string;
+  @Input() public year!: string;
 }
